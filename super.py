a3fdcb8ba6db4afaeff558e5714c814cd8231d0e
Noora Panahi <nourap064@gmail.com>
2025-06-09 09:23:28 -0700

    shopping

---
+++ super.py
@@ -0,0 +1,21 @@
+foodList = {"milk" : 10 , "chocolate" : 50 , "burger" : 35 , "tea" : 40 , "banana" : 25}
+selectedFoodList = []
+totalPrice = 0
+
+for key,values in foodList.items():
+    print(f"Food {key} price : {values}")
+
+try:
+    while True:
+        selectedFood = input("choose the food you want: ")
+        if selectedFood in foodList:
+            selectedFoodList.append(selectedFood)
+            selectedFoodListPrice = foodList[selectedFood]
+            totalPrice += selectedFoodListPrice
+        else:
+            print("not available!")
+except KeyboardInterrupt as e:
+    print("--------------")
+    print(f"your list : {selectedFoodList} , {totalPrice} toman.")
+    with open("List.txt" , "w") as file :
+        file.write(f"your list : {selectedFoodList} , {totalPrice} toman.")
